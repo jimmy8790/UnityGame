bd19c7d108c92d6340802e49eb30ed2596dc5e53
jimmy <129704359+jimmy8790@users.noreply.github.com>
2025-03-25 14:16:16 +0900

    update

--- AppleControl.cs
+++ AppleControl.cs
@@ -3,6 +3,14 @@ using UnityEngine;
 
 public class AppleControl : MonoBehaviour
 {
+    private Sprite apple1;
+    public Sprite apple2;
+    private SpriteRenderer appleSprite;
+
+    void Start(){
+        appleSprite = GetComponent<SpriteRenderer>();
+        apple1 = appleSprite.sprite;
+    }
     private Color[] myColor = {
         Color.red,
         new Color(1f,0.5f,0f),
@@ -16,10 +24,15 @@ public class AppleControl : MonoBehaviour
     private int colorIndex=0;
     void OnTriggerEnter2D(Collider2D other)
     {
+        appleSprite.sprite = apple2;
+        Invoke("origin",0.5f);
+
         SpriteRenderer playSprite = other.GetComponent<SpriteRenderer>();
         playSprite.color = myColor[colorIndex];
         colorIndex=(colorIndex+1)%myColor.Length;
-
+    }
+    void origin(){
+        appleSprite.sprite = apple1;
         float newX = Random.Range(-10,10);
         float newY = Random.Range(-4,4);
         transform.position = new Vector3(newX,newY,0);
